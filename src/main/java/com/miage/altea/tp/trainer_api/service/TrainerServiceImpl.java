a4473aafd95ca17fdaf448c1e765cecefdad94d7
ouikene koceila <koceila.ouikene@worldline.com>
2019-02-09 22:31:43 +0100

    First Implementation 1 to 6.4.2 All tests works :)

---
+++ src/main/java/com/miage/altea/tp/trainer_api/service/TrainerServiceImpl.java
@@ -0,0 +1,35 @@
+package com.miage.altea.tp.trainer_api.service;
+
+import com.miage.altea.tp.trainer_api.bo.Trainer;
+import com.miage.altea.tp.trainer_api.repository.TrainerRepository;
+
+import java.util.Optional;
+
+public class TrainerServiceImpl implements TrainerService{
+    private TrainerRepository trainerRepository;
+
+    public TrainerServiceImpl(TrainerRepository trainerRepository) {
+        this.trainerRepository = trainerRepository;
+    }
+
+    @Override
+    public Iterable<Trainer> getAllTrainers() {
+        return trainerRepository.findAll();
+    }
+
+    @Override
+    public Trainer getTrainer(String name) {
+        Optional<Trainer> result = trainerRepository.findById(name);
+        final Trainer[] t = {null};
+
+        result.ifPresent(trainer -> {
+            t[0] = trainer;});
+
+        return t[0];
+    }
+
+    @Override
+    public Trainer createTrainer(Trainer trainer) {
+        return trainerRepository.save(trainer);
+    }
+}
